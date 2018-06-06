e3eb4777906323c392f89877e80083e23ba0451f
trentduncan <trent.dunc@gmail.com>
2018-06-06 14:12:51 -0700

    importing schema from a different file and connected to mongoose

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+const schema = require('./schema');
+
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+mongoose.connect('mongodb://localhost/blog-app-graphql');
+const db = mongoose.connection;
+db.on('error', ()=> {console.log( '---FAILED to connect to mongoose');});
+db.once('open', () => {
+  console.log( '+++Connected to mongoose');
+});
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
