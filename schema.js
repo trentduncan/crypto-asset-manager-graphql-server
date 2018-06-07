0a084a723597337df6f0034bc3c7154c1974b0c3
trentduncan <trent.dunc@gmail.com>
2018-06-06 17:06:28 -0700

    added schema for crypto currencies

--- schema.js
+++ schema.js
@@ -3,22 +3,49 @@
 const { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
+// const schema = buildSchema(`
+//   type Query {
+//     hello: String
+//   }
+//   type User {
+//     id: ID
+//     firstName: String!
+//     lastName: String!
+//     posts: [Post]
+//   }
+
+//   type Post {
+//     id: ID
+//     title: String
+//     content: String
+//     author: User
+//   }
+// `);
 const schema = buildSchema(`
   type Query {
     hello: String
-  }
-  type User {
-    id: ID
-    firstName: String!
-    lastName: String!
-    posts: [Post]
+    getUser(username: String): User
+    getTop10(sort: String): [Ticker]
   }
 
-  type Post {
+  type Ticker {
+    name: String
+    symbol: String
+    change1h: Int
+    change24h: Int
+    change7d: Int
+  }
+
+  type User {
     id: ID
-    title: String
-    content: String
-    author: User
+    username: String!
+    coins: [Coin]
+  }
+
+  type Coin {
+    id: Int
+    symbol: String
+    amount: Int
   }
 `);
 
